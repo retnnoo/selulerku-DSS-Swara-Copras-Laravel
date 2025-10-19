6c094ac0cc870d97d988615e1fd4fbe40cec5c0a
retnnoo <retnowardani024@gmail.com>
2025-10-19 15:06:12 +0700

    font

--- resources/views/admin/login_admin.blade.php
+++ resources/views/admin/login_admin.blade.php
@@ -4,7 +4,10 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Login</title>
+  <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
   @vite('resources/css/app.css')
+  
 </head>
 <body class="min-h-screen flex items-center justify-center bg-gray-50">
 
