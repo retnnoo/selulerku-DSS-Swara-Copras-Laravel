cc9ad37df24abfb859088d077ea31ad79206df88
retnnoo <retnowardani024@gmail.com>
2025-11-12 11:01:17 +0700

    panggil js di swara

--- resources/views/admin/pembobotan_swara.blade.php
+++ resources/views/admin/pembobotan_swara.blade.php
@@ -68,6 +68,7 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <!-- DataTables JS -->
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="{{ asset('/js/script.js') }}"></script>
 
 </body>
 </html>
